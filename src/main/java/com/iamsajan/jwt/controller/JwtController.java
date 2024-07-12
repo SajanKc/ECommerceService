309985a676e08b1fc31c92c22836ae473b40a911
sajan96kc <sajan1996kc@gmail.com>
2024-07-12 20:43:30 +0545

    initial project setup with jwt

---
+++ src/main/java/com/iamsajan/jwt/controller/JwtController.java
@@ -0,0 +1,23 @@
+package com.iamsajan.jwt.controller;
+
+import com.iamsajan.jwt.entity.JwtRequest;
+import com.iamsajan.jwt.entity.JwtResponse;
+import com.iamsajan.jwt.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class JwtController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping({"/authenticate"})
+    public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        return jwtService.createJwtToken(jwtRequest);
+    }
+}
---
+++ src/test/java/com/iamsajan/jwt/ECommerceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamsajan.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
